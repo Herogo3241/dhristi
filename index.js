9d675a68c844da6d6d0efe290f282a1e702f4fc8
Herogo3241 <goureesh.chandra23@gmail.com>
2024-03-29 18:40:24 +0530

    add debug

--- index.js
+++ index.js
@@ -83,10 +83,11 @@ async function makeRequest() {
 function runIfBetween6And7() {
     var currentTime = new Date();
     var currentHour = currentTime.getHours();
+    console.log(currentHour);
 
     // Check if the current hour is between 18:00 (6pm) and 19:00 (7pm)
     if (currentHour >= 18 && currentHour < 19) {
-        console.log(currentHour);
+        
         makeRequest();
     } else {
         console.log("Current time is not between 6pm and 7pm.");
